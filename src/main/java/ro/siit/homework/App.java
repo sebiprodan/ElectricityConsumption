38e08de2e4cc155baf50bae84b4acaf8c2d5de1f
Sebastian Prodan <sebiprodan@gmail.com>
2018-01-29 21:07:11 +0200

    The Dryer is created and functional.

--- src/main/java/ro/siit/homework/App.java
+++ src/main/java/ro/siit/homework/App.java
@@ -3,19 +3,11 @@ package ro.siit.homework;
 public class App {
     public static void main(String[] args) {
 
-        DishWasher dw1 = new DishWasher("LG");
-        System.out.println(dw1.consumption());
-        dw1.start();
-        System.out.println(dw1.consumption());
-        dw1.stop();
-        System.out.println(dw1.consumption());
-        dw1.stop();
-        System.out.println(dw1.consumption());
+        Dryer dr1 = new Dryer("Philips");
+        System.out.println(dr1.consumption());
 
-
-
-
-
+        dr1.decreaseAirPower();
+        dr1.stop();
 
 
     }
--- src/main/java/ro/siit/homework/Dryer.java
+++ src/main/java/ro/siit/homework/Dryer.java
@@ -2,15 +2,59 @@ package ro.siit.homework;
 
 public class Dryer extends Appliance {
 
-    public void stop() {
+    protected int airHeatLevel;
+    protected int airPower;
 
+    public Dryer(String brandName) {
+        this.brandName = brandName;
+        this.consumptionPerHour = 0;
+        this.airPower = 0;
+        this.airHeatLevel = 0;
     }
 
     public void start() {
+        if (this.consumptionPerHour > 0) {
+            System.out.println("The Dryer is already ON");
+        } else {
+            this.consumptionPerHour = 250;
+            this.airHeatLevel = 1;
+            this.airPower = 1;
+        }
+    }
 
+    public void increaseAirPower() {
+        if (this.consumptionPerHour == 0) {
+            System.out.println("The Dryer is OFF. Please turn it ON");
+        } else if (this.consumptionPerHour == 750) {
+            System.out.println("This Dryer has only 3 air power levels. Please use one of these 3");
+        } else {
+            this.consumptionPerHour = this.consumptionPerHour + 250;
+            this.airPower = airPower + 1;
+            this.airHeatLevel = this.airHeatLevel + 1;
+        }
+    }
+
+    public void decreaseAirPower() {
+        if (this.consumptionPerHour == 0) {
+            System.out.println("The Dryer is OFF. Please turn it ON");
+        } else {
+            this.consumptionPerHour = this.consumptionPerHour - 250;
+            this.airPower = airPower - 1;
+            this.airHeatLevel = this.airHeatLevel - 1;
+        }
+    }
+
+    public void stop() {
+        if (this.consumptionPerHour == 0) {
+            System.out.println("The Dryer is already OFF");
+        } else {
+            this.consumptionPerHour = 0;
+            this.airHeatLevel = 0;
+            this.airPower = 0;
+        }
     }
 
     public String consumption() {
-        return this.brandName + " Dryer: " + this.consumptionPerHour;
+        return this.brandName + " Dryer: " + this.consumptionPerHour + "Wh";
     }
 }
